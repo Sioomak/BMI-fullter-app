6b7636394f3099a7444bf547deb5e7ccdacd900f
Siamak Salimi <ssalimi1220@gmail.com>
2020-09-01 15:57:08 -0400

    all constants moved to a separate dart file

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -92,12 +92,14 @@ class _InputPageState extends State<InputPage> {
                   ),
                   Slider(
                     value: height.toDouble(),
-                    min: 120,
-                    max: 220,
+                    min: 120.0,
+                    max: 220.0,
                     activeColor: Color(0xFFEB1555),
                     inactiveColor: Color(0xFF8D8E98),
                     onChanged: (double newValue) {
-                      print(newValue);
+                      setState(() {
+                        height = newValue.round();
+                      });
                     },
                   ),
                 ],
