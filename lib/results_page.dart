dfbcf5f4f154b7cabf6e8ad50d9024992f8a5349
Siamak Salimi <ssalimi1220@gmail.com>
2020-09-02 14:49:01 -0400

    results page items before creating the button

--- lib/results_page.dart
+++ lib/results_page.dart
@@ -1,4 +1,6 @@
+import 'package:bmi_calculator/constants.dart';
 import 'package:flutter/material.dart';
+import 'reusable_card.dart';
 
 class ResultsPage extends StatelessWidget {
   @override
@@ -7,7 +9,42 @@ class ResultsPage extends StatelessWidget {
       appBar: AppBar(
         title: Text('BMI CALCULATOR'),
       ),
-      body: Text('Hello!'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Text(
+                'Your Results',
+                style: kTitleTextStyle,
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: ReusableCard(
+              colour: kActiveCardsColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'Normal',
+                    style: kResultTextStyle,
+                  ),
+                  Text(
+                    '18.9',
+                    style: kBmiTextStyle,
+                  ),
+                  Text('Your BMI result is low, you should eat more!',
+                      textAlign: TextAlign.center, style: kBmiInterpretStyle),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
